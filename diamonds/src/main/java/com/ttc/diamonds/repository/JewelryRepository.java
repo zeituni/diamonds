4fb420d355acbb5a43a6b44bf210e980a8dcb024
zeituni <zeituni@gmail.com>
2018-11-25 11:01:58 +0200

    fix manufacturer bug

--- diamonds/src/main/java/com/ttc/diamonds/repository/JewelryRepository.java
+++ diamonds/src/main/java/com/ttc/diamonds/repository/JewelryRepository.java
@@ -1,12 +1,13 @@
 package com.ttc.diamonds.repository;
 
 import com.ttc.diamonds.model.Jewelry;
+import com.ttc.diamonds.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface JewelryRepository extends JpaRepository<Jewelry, Long> {
 
-    List<Jewelry> findByManufacturer(Long manufacturerId);
+    List<Jewelry> findByManufacturer(Manufacturer manufacturer);
     Jewelry findByBarcode(String barcode);
 }
