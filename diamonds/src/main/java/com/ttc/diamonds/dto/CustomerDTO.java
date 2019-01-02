2290e8ed92a667f27752907a15b4d7464b7b6d0f
zeituni <zeituni@gmail.com>
2019-01-02 23:47:34 +0200

    Add customer api

---
+++ diamonds/src/main/java/com/ttc/diamonds/dto/CustomerDTO.java
@@ -0,0 +1,82 @@
+package com.ttc.diamonds.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Objects;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomerDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String barcode;
+    private String videoUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomerDTO)) return false;
+        CustomerDTO that = (CustomerDTO) o;
+        return Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getEmail(), that.getEmail()) &&
+                Objects.equals(getPhone(), that.getPhone()) &&
+                Objects.equals(getBarcode(), that.getBarcode());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId(), getName(), getEmail(), getPhone(), getBarcode());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
