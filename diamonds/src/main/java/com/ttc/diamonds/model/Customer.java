91722b7d4f69972a468450c5ad5c92e0eb31ca68
zeituni <zeituni@gmail.com>
2019-01-02 23:48:26 +0200

    Add customer api

---
+++ diamonds/src/main/java/com/ttc/diamonds/model/Customer.java
@@ -0,0 +1,80 @@
+package com.ttc.diamonds.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    @ManyToOne
+    @JoinColumn(name = "manufacturer", referencedColumnName = "id")
+    private Manufacturer manufacturer;
+    @ManyToOne
+    @JoinColumn(name = "jewelry", referencedColumnName = "id")
+    private Jewelry jewelry;
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Jewelry getJewelry() {
+        return jewelry;
+    }
+
+    public void setJewelry(Jewelry jewelry) {
+        this.jewelry = jewelry;
+    }
+}
