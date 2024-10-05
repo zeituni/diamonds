752f1380a0631a7f6c99d9a5708a7e046af37396
Uri <zeituni@gmail.com>
2024-10-05 19:11:06 +0300

    fix user first name and last name

--- diamonds/src/main/java/com/ttc/diamonds/dto/UserDTO.java
+++ diamonds/src/main/java/com/ttc/diamonds/dto/UserDTO.java
@@ -7,9 +7,7 @@ import java.util.Objects;
 public class UserDTO {
 
     private Long id;
-    @JsonProperty(value = "first_name")
     private String firstName;
-    @JsonProperty(value = "last_name")
     private String lastName;
     private String username;
     private String password;
